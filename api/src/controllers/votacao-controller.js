8d1f93deb29fda3b20225b09fe98d2fcc8c05f84
Guilherme Neves <guineves1997@gmail.com>
2019-12-04 00:29:08 -0300

    .

--- api/src/controllers/votacao-controller.js
+++ api/src/controllers/votacao-controller.js
@@ -43,7 +43,7 @@ exports.readVotacao = (req, res) => {
 
     votacaoNet.readVotacao(query.nomeVotacao).then(
         resp => {
-            res.json(resp);
+            res.json(JSON.stringify(resp));
         }, err => {
             res.status(400).send(err);
         }
